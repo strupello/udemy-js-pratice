1b5e3ef9e6b2088f944db04db325758f106bfe97
strupello <irek1992@gmail.com>
2025-11-12 23:04:58 +0100

    complete lesson 16-22 - pratice with chatgpt - basic math examples

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+let x = 10;
+let y = 4;
+
+let result = x + y;
+console.log(result);
+
+let currentYear = 2025;
+let birthYear = 1992;
+
+let age = currentYear - birthYear;
+
+console.log("Masz " + age + " lat!");
+
+let price =  120;
+let tax = 0.23;
+
+let finalPrice = price + (price * tax);
+console.log("Cena końcowa:" + finalPrice );
+
+let a = 7;
+let b = 3;
+let c = 2;
+
+let operationResult = ( a + b ) * c;
+console.log(operationResult);
